0802bc8cf61cd77b0c63c9318d4afa90666c42f1
jhuan102 <300818592@cencol.ca>
2017-01-21 15:47:27 -0500

    Added the functions

---
+++ Connect Example/server.js
@@ -0,0 +1,35 @@
+//your server will live here
+//import the 3rd party connect module
+//assign it as a reference to the connect variable
+let connect = require('connect');
+
+//create an instance of the connect server - app
+let app = connect();
+
+//port constant
+const port = 3000;
+
+//start listening
+app.listen(port);
+console.log(`Server Started at http://localhost:$(port)`);
+
+function home (req, res, next)
+{
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(' ');
+
+    next();
+}
+
+function hello(req,res,next)
+{
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+
+    next();
+}
+
+//Routing
+//First Route '/'
+app.use('/hello', helloWorld);
+app.use('/', home);
